da568836aa5f2575d8db323302bc61215ecb5763
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-08-11 15:31:05 +0530

    Add home route

--- app.js
+++ app.js
@@ -97,6 +97,9 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/" , userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 main()
 .then(() => {
