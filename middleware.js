db2676808b245dd655177649f27491348cf33e9b
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-08-11 15:50:45 +0530

    Restrict review deletion to review author

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const Listing = require("./models/listing");
 const{listingSchema,reviewSchema} = require("./schema.js");
 const ExpressError = require("./utils/ExpressError.js");
+const Review = require("./models/review.js");
 
 
 module.exports.isLoggedIn = (req, res, next)=>{
@@ -56,12 +57,15 @@ module.exports.validateReview = async(req, res, next)=>{
     };
 
 
-module.exports.isReviewAuthor = async(req, res, next)=>{
-    let {id, reviewId} = req.params;
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+
     let review = await Review.findById(reviewId);
-    if(!review.author._id.equals(res.locals.currUser._id)){
-        req.flash("error","You are not the author of this review!");
+
+    if (!review.author.equals(req.user._id)) {
+        req.flash("error", "You are not the author of this review!");
         return res.redirect(`/listings/${id}`);
     }
+
     next();
-    };
+};
